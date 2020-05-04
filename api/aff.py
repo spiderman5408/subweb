ac0ebbfa3c620c3a8ca4da9676293f27ed6278f9
spiderman5408 <55320058+spiderman5408@users.noreply.github.com>
2020-05-04 19:44:34 +0800

    1
    1

--- api/aff.py
+++ api/aff.py
@@ -1,7 +1,7 @@
 #全局变量定义
-subip = 'http://127.0.0.1:10010'      # 默认subip是 subconverter 后台端口，在config/perf.ini 中指定，或者docker的端口指定。
-apiip = 'http://127.0.0.1:10086'      #apiip 是 suweb 前端端口
-passwd = 'admin'                      #passwd 是 admin系统的密码
+subip = 'http://hd.clash008.top'      # 默认subip是 subconverter 后台端口，在config/perf.ini 中指定，或者docker的端口指定。
+apiip = 'https://ss.clash008.top'      #apiip 是 suweb 前端端口
+passwd = '17733906174a.'                      #passwd 是 admin系统的密码
 #proxygroup  用于节点分组，当您修改pref.ini的 [ruleset] 需要改变以下默认值以一一对应，否则会导致节点分组报错。
 proxygroup= '@🔰 节点选择`select{groupname}[]DIRECT'\
             '@📲 电报吹水`select`[]🔰 节点选择{groupname}[]DIRECT'\
